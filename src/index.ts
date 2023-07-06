2f525538ad545c943c5d43df4cbc9ce0d9006df2
decaplanet <126875757+decaplanet@users.noreply.github.com>
2023-07-06 19:41:33 +0900

    feat: add checkJsonExists function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import checkJsonExistsImport from "./tools/checkJsonExists";
 import modifyJsonImport from "./tools/modifyJson";
 import readJsonImport from "./tools/readJson";
 
 export const modifyJson = modifyJsonImport;
 export const readJson = readJsonImport;
+export const checkJsonExists = checkJsonExistsImport;

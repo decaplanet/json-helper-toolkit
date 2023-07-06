2bf18968262f0524360e90f6a3d0234464f788ef
decaplanet <126875757+decaplanet@users.noreply.github.com>
2023-07-06 19:40:32 +0900

    fix: return null when the reading data is not valid

--- src/tools/readJson.ts
+++ src/tools/readJson.ts
@@ -3,14 +3,18 @@ import path from "path";
 
 export type ReadJsonFunction = <T = any>(
 	inputPath: string
-) => [path: string, data: T];
+) => [path: string, data: T | null];
 
 const readJson: ReadJsonFunction = (pathInput) => {
 	const callerPath = process.cwd();
 	const outputPath = path.resolve(callerPath, pathInput);
 	const outputData = JSON.parse(fs.readFileSync(outputPath, "utf8"));
 
-	return [outputPath, outputData];
+	if (outputData) {
+		return [outputPath, outputData];
+	} else {
+		return [outputPath, null];
+	}
 };
 
 export default readJson;
